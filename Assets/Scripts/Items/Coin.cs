7feb6f1a68134c33e3a6f8a09c38bc71911bb04d
Pex-Dev <brayanc.moya@gmail.com>
2026-04-20 15:57:52 -0400

    Añadir sonidos a moneda

--- Assets/Scripts/Items/Coin.cs
+++ Assets/Scripts/Items/Coin.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] coinSounds;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {   
+            if(coinSounds.Length>0)ScenesManager.instance.PlaySound(coinSounds[Random.Range(0,coinSounds.Length)]);
             collision.gameObject.GetComponent<ObjectAbsorption>()?.AddCoins(5);
             Destroy(gameObject);
         }
